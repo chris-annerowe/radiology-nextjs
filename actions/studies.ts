dff223c29288c713e070427838016ae6f0adb899
unknown <chris-anne.rowe@aye-tech.com>
2024-08-01 11:08:58 -0500

    Add patient_studies table to form relation between studies and patients. Table shows the studies ordered for a patient_id

--- actions/studies.ts
+++ actions/studies.ts
@@ -12,3 +12,27 @@ export const findAllStudies = async () => {
     return studies;
 
 }
+
+export const findStudyByPatientId = async (patientId:string) => {
+    const studies = await db.patient_studies.findMany({
+        where: {
+            patient_id: patientId
+        }
+    })
+    return studies
+}
+
+export const addPatientStudy = async (patient_id:string, study_id:number, study_name: string, cpt_code:string) => {
+    try{
+        await db.patient_studies.create({
+            data: {
+                patient_id,
+                study_id,
+                study_name,
+                cpt_code
+            }
+        })
+        console.log("Patient study created successfully")
+    }catch(e){ throw e }
+    
+}
