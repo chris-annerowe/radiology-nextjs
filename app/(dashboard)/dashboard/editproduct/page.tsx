337801a20039b39ae6a8286fd228d0442c84b833
unknown <chris-anne.rowe@aye-tech.com>
2025-12-16 18:01:42 -0500

    Remove unused code

--- app/(dashboard)/dashboard/editproduct/page.tsx
+++ app/(dashboard)/dashboard/editproduct/page.tsx
@@ -1,22 +1,18 @@
 'use client'
 
-import { addPatientStudy, findAllStudies } from "@/actions/studies";
-import { Patient } from "@/types/patient";
+import { findAllStudies } from "@/actions/studies";
 import { Study } from "@/types/studies";
 import EditStudyModal from "@/ui/dashboard/accessioning/studies/edit-study-modal";
-import { AutoComplete } from "antd";
-import { Modal, Pagination, Table, TextInput } from "flowbite-react";
+import { Pagination, Table } from "flowbite-react";
 import { useEffect, useState } from "react";
 
 
 
 export default function EditStudy() {
 
-    //const patients: Patient[] = [];
-
     const [openEditModal, setOpenEditModal] = useState(false)
     const [selectedStudy, setSelectedStudy] = useState<Study>({
-        study_id: 2n,
+        study_id: 0,
         cpt_code: '',
         study_name: null, 
         study_description: null,
@@ -25,24 +21,7 @@ export default function EditStudy() {
         isInsurable: null,
         isTaxable: null
     })
-    // const [errors, setErrors] = useState<{ [key: string]: any }>({});
-
-    // const [options, setOptions] = useState<{ value: string }[]>([{ value: "Apple" }, { value: "Orange" }]);
-
-    // const limit = 5;
-
-
-    // const [studySearch, setStudySearch] = useState<{
-    //     cptCode: string,
-    //     studyName: string,
-    //     modality: string
-    // }>({
-    //     cptCode: "",
-    //     studyName: "",
-    //     modality: ""
-    // })
-
-
+    
     const [activePage, setActivePage] = useState(1);
 
     const [totalPages, setTotalPages] = useState(1);
@@ -52,22 +31,6 @@ export default function EditStudy() {
     useEffect(()=>{
         getStudiesList();
     },[])
-
-
-
-    // const searchPatients = async (e: React.KeyboardEvent<HTMLInputElement>) => {
-    //     const search = e.currentTarget.value;
-    //     /* if (e.key === "Enter") {
-    //          let result = await findPatientByName(search, activePage, limit);
- 
-    //          if (result.data) {
-    //              setPatients(result.data);
-    //              setTotalPages(result.pagination.count / limit);
-    //          }
-    //      }*/
-
-
-    // };
 
     const closeEditModal = () => {
         setOpenEditModal(false);
@@ -103,22 +66,6 @@ export default function EditStudy() {
             <EditStudyModal open={openEditModal} onClose={closeEditModal} study={selectedStudy} />
                <div className="space-y-6">
                         <h3 className="text-xl font-medium text-gray-900 dark:text-white">Edit Study</h3>
-
-
-                        {/* <div className="flex space-x-2">
-                            <TextInput id="cpt_code" type="text" placeholder="CPT Code" className="" />
-                            <TextInput id="study_name" type="text" placeholder="Study Name" className="" />
-                            <AutoComplete id="modality_code_ac" options={options}  className="">
-                                <TextInput id="modality_code" type="text" placeholder="Modality" className="" />
-                            </AutoComplete>
-                            {/*<TextInput id="first_name" name="first_name" type="" placeholder="First Name" color={errors?.last_name ? "failure" : "gray"} onChange={handlePatientSearchChange} className="" />
-                            <TextInput id="last_name" name="last_name" type="" placeholder="Last Name" color={errors?.first_name ? "failure" : "gray"} onChange={handlePatientSearchChange} className="" />*/}
-                            {/*<Datepicker name="dob" value={patientSearch.dob.toDateString()} onSelectedDateChanged={(date) => handleDOBDateChange(date)}  />*/}
-                            {/*<Button onClick={() => { searchPatients(patientSearch.firstName) }}>
-                                <HiSearch className="mr-2 h-5 w-5" />
-                                Search
-                        </Button>*/}
-                        {/* </div>  */}
 
                         <Table striped hoverable>
                             <Table.Head>
